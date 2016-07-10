7ad90dfeaa39bc73407b7a7213ef1a87bdca54e7
Gadiel Ortez <gorte@agilesolutions.com>
2016-07-10 08:28:47 -0600

    home finished

--- App/Containers/PresentationScreen.js
+++ App/Containers/PresentationScreen.js
@@ -4,6 +4,7 @@ import { Images } from '../Themes'
 import { connect } from 'react-redux'
 import Routes from '../Navigation/Routes'
 import RoundedButton from '../Components/RoundedButton'
+import Icon from 'react-native-vector-icons/FontAwesome'
 //import I18n from '../I18n/I18n.js'
 
 // Styles
@@ -86,11 +87,27 @@ export default class PresentationScreen extends React.Component {
   
   }
 
-  renderItem(rowData){
+  clickedRoom(roomData){
+
+  }
+
+  renderItem(roomDetails){
 
     return(
-      <TouchableOpacity onPress={()=>this.setIcon(rowData)}>
-        
+      <TouchableOpacity onPress={()=>this.clickedRoom(roomDetails)}>
+        <View style={{marginHorizontal:15,height:60, flexDirection:'row', alignItems:'center', borderBottomWidth:0.5, }}>
+          <View style={{marginRight:10,height:40, width:40,justifyContent:'center',backgroundColor:'red', alignItems:'center', borderRadius:100}}>
+            <Icon name={roomDetails.icon} size={22} color='#fff' style={{alignSelf:'center'}}/>
+          </View>
+          <View style={{flex:8}}>
+            <Text style={{fontSize:13}}> {roomDetails.name} </Text>
+            <Text style={{fontSize:10}}> {roomDetails.location.lat} </Text>
+          </View>
+          <View style={{marginRight:10,flexDirection:'row',alignItems:'center', justifyContent:'center',flex:1,padding:7, borderRadius:30, backgroundColor:'#E7E7E7'}} >
+            <View style={{borderRadius:30,height:5, width:5,marginRight:4, backgroundColor:'green'}}/>
+            <Text>{roomDetails.people}</Text>
+          </View>
+        </View>
       </TouchableOpacity>
     );
   }
@@ -105,7 +122,7 @@ export default class PresentationScreen extends React.Component {
     return (
       <View style={styles.container}>
 
-        <View style={{flexDirection:'row',alignItems:'center',height:50, backgroundColor:'#4F96DB'}}>
+        <View style={{marginBottom:15,flexDirection:'row',alignItems:'center',height:50, backgroundColor:'#4F96DB'}}>
           
           <View style={{height:60, width:65}}/>
           <Text style={{flex:10,color:'white',fontSize:20}}>{this.state.username}</Text>
@@ -121,6 +138,9 @@ export default class PresentationScreen extends React.Component {
           automaticallyAdjustContentInsets={false}
           keyboardDismissMode="on-drag"
           keyboardShouldPersistTaps={true}/>
+        <View style={{marginLeft:10,height:40}}>
+          <Text style={{fontSize:20}} >VIEW CHANNELS AROUND 5kms</Text>
+        </View>
       </View>
     )
   }
